a56e0368f60781cc4dbb81b6ecd4357aa1fbfb89
魏胜楠 <shengnan.wei@hand-china.com>
2016-12-07 19:14:40 +0800

    add cache

---
+++ consumerBanking/src/main/java/hybris/App.java
@@ -0,0 +1,13 @@
+package hybris;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+{
+Util.printMessage("Hello World!");
+}
+}
---
+++ consumerBanking/src/main/java/hybris/Util.java
@@ -0,0 +1,12 @@
+package hybris;
+
+/**
+ * Hello world!
+ *
+ */
+public class Util
+{
+public static void printMessage(String message){
+System.out.println(message);
+}
+}
